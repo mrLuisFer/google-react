1d1fe5356e81d5d5cc1b26ac167bc2676e4e7ef6
mrLuisFer <lolesuncrak@gmail.com>
2020-10-31 19:32:12 -0600

    [ADD] Adding the docs dir in the project

---
+++ docs/precache-manifest.08c9fadf1efad506dbd73df48e416245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8717e7501684eee0af31371fca40517e",
+    "url": "/google-react/index.html"
+  },
+  {
+    "revision": "9581104fe07d35b2b488",
+    "url": "/google-react/static/css/main.5a8ad2f6.chunk.css"
+  },
+  {
+    "revision": "e02d43f38ee6588c40cc",
+    "url": "/google-react/static/js/2.434101f0.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/google-react/static/js/2.434101f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9581104fe07d35b2b488",
+    "url": "/google-react/static/js/main.6069bfbc.chunk.js"
+  },
+  {
+    "revision": "8d514c557c3ddbed2b82",
+    "url": "/google-react/static/js/runtime-main.71ea58a3.js"
+  },
+  {
+    "revision": "e99543bb86794e15928d03e9b41bb3c8",
+    "url": "/google-react/static/media/Google_logo.e99543bb.svg"
+  }
+]);
