5c19a34348ab41890c85b8f115699c2dea5e6465
mrLuisFer <mrluisfeer@gmail.com>
2021-08-22 18:27:32 -0500

    fix: separating the footer link in a component

--- src/components/FooterLink.tsx
+++ src/components/FooterLink.tsx
@@ -7,7 +7,8 @@ type Props = {
 
 export default function FooterLink ({text, url}: Props) {
   return (
-
+  <p className="pr-5 cursor-pointer select-none leading-6 hover:underline">
+    <a href={url} target="_blank" rel="noopener noreferrer" >{text}</a> </p>
   )
 }
 
