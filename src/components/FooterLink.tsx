638f5f7847875361ec85d1b25ebedde414414228
mrLuisFer <mrluisfeer@gmail.com>
2021-08-22 18:42:39 -0500

    feat: adding prettier options and script in package json

--- src/components/FooterLink.tsx
+++ src/components/FooterLink.tsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React from 'react'
 
 type Props = {
   text: string
   url: string
 }
 
-export default function FooterLink ({text, url}: Props) {
+export default function FooterLink({ text, url }: Props) {
   return (
-  <p className="pr-5 cursor-pointer select-none leading-6 hover:underline">
-    <a href={url} target="_blank" rel="noopener noreferrer" >{text}</a> </p>
+    <p className="pr-5 cursor-pointer select-none leading-6 hover:underline">
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        {text}
+      </a>{' '}
+    </p>
   )
 }
-
