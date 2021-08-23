6eb743b7854c1591e40de69c2a2f4b7d8c23f074
mrLuisFer <mrluisfeer@gmail.com>
2021-08-22 21:04:18 -0500

    chore: updating tailwind to latest version
    feat: adding some dependencies for tailwindcss

--- src/components/GoogleSearches.tsx
+++ src/components/GoogleSearches.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function GoogleSearches() {
   return (
-    <div>
+    <div className="absolute p-6 bg-white z-10 w-full h-40 rounded-2xl border-gray-200 border-2 shadow-sm top-10">
       <p>Busquedas</p>
     </div>
   )
